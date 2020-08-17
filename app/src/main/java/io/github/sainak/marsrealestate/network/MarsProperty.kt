436cec6dbee649625c9674bc4e7d78af779b3182
sainAk <aakashsingh1407@gmail.com>
2020-08-17 09:33:32 +0530

    annotated data class to exclude from proguard

--- app/src/main/java/io/github/sainak/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/io/github/sainak/marsrealestate/network/MarsProperty.kt
@@ -1,8 +1,9 @@
 package io.github.sainak.marsrealestate.network
 
+import androidx.annotation.Keep
 import com.squareup.moshi.Json
 
-
+@Keep
 data class MarsProperty(
     val id: String,
     @Json(name = "img_src") val imgSrcUrl: String,
