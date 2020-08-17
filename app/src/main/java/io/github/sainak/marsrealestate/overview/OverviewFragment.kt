be54d751b475c2c2b22b58c19c82ca9d74b9ff4e
sainAk <aakashsingh1407@gmail.com>
2020-08-17 20:41:51 +0530

    added buy, rent, all filters from api query parameters

--- app/src/main/java/io/github/sainak/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/io/github/sainak/marsrealestate/overview/OverviewFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import io.github.sainak.marsrealestate.R
 import io.github.sainak.marsrealestate.databinding.FragmentOverviewBinding
+import io.github.sainak.marsrealestate.network.MarsApiFilter
 
 /**
  * This fragment shows the the status of the Mars real-estate web services transaction.
@@ -47,5 +48,17 @@ class OverviewFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.overflow_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        viewModel.updateFilter(
+            when (item.itemId) {
+                R.id.show_rent_menu -> MarsApiFilter.SHOW_RENT
+                R.id.show_buy_menu -> MarsApiFilter.SHOW_BUY
+                else -> MarsApiFilter.SHOW_ALL
+            }
+        )
+        return true
     }
 }
