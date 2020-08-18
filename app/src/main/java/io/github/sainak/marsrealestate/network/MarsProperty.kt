97fc2892493e5f3901cedbb68ea2c1ab409131b2
sainAk <aakashsingh1407@gmail.com>
2020-08-18 09:11:51 +0530

    Parcelize MarsProperty Data class
    fixes crashes (not able to inflate fragments from nav graph)

--- app/src/main/java/io/github/sainak/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/io/github/sainak/marsrealestate/network/MarsProperty.kt
@@ -1,15 +1,22 @@
 package io.github.sainak.marsrealestate.network
 
-import androidx.annotation.Keep
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
-@Keep
+/**
+ * This data class defines a Mars property which includes an ID, the image URL, the type (sale
+ * or rental) and the price (monthly if it's a rental).
+ * The property names of this data class are used by Moshi to match the names of values in JSON.
+ */
+@Parcelize
 data class MarsProperty(
     val id: String,
+    // used to map img_src from the JSON to imgSrcUrl in our class
     @Json(name = "img_src") val imgSrcUrl: String,
     val type: String,
     val price: Double
-) {
-
-    val isRental get() = type == "rent"
+) : Parcelable {
+    val isRental
+        get() = type == "rent"
 }
